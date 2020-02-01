ae3fa88e35454e75e2bd5fd90d1bd6e011aedbff
kashan siddiqui <kashanzahoorsiddiqui@gmail.com>
2020-02-01 16:23:11 +0500

    kraken added and rapid api

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { StripeModule } from './stripe/stripe.module';
 import { NodemailerModule } from './nodemailer/nodemailer.module';
 import { AuthModule } from './auth/auth.module';
 import { PaypalModule } from './paypal/paypal.module';
+import { KrakenModule } from './kraken/kraken.module';
 
 
 
@@ -17,6 +18,7 @@ import { PaypalModule } from './paypal/paypal.module';
     NodemailerModule,
     PaypalModule,
     AuthModule,
+    KrakenModule,
     MongooseModule.forRoot('mongodb+srv://hasanrasheed:Pass123456@cluster0-0kmmw.mongodb.net/test?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
