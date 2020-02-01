eae571ba3cea039494d87f71dab916f9fb25779b
kashan siddiqui <kashanzahoorsiddiqui@gmail.com>
2020-02-01 15:53:28 +0500

    paypal added

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ProductModule } from './products/product.module';
 import { StripeModule } from './stripe/stripe.module';
 import { NodemailerModule } from './nodemailer/nodemailer.module';
 import { AuthModule } from './auth/auth.module';
+import { PaypalModule } from './paypal/paypal.module';
 
 
 
@@ -14,6 +15,7 @@ import { AuthModule } from './auth/auth.module';
     ProductModule,
     StripeModule,
     NodemailerModule,
+    PaypalModule,
     AuthModule,
     MongooseModule.forRoot('mongodb+srv://hasanrasheed:Pass123456@cluster0-0kmmw.mongodb.net/test?retryWrites=true&w=majority')],
   controllers: [AppController],
