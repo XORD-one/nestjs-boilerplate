e08631b985878c31de0c5d81e7e7969cb92c6ec4
sarfarazahmedkhan <sarfarazahmedkhankhan@gmail.com>
2020-10-21 15:50:29 +0500

    title: include multer package description:upload mulitple or single images in nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,28 @@
-import { Module } from '@nestjs/common';
-import {MongooseModule} from '@nestjs/mongoose'
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductModule } from './products/product.module';
-import { StripeModule } from './stripe/stripe.module';
-import { NodemailerModule } from './nodemailer/nodemailer.module';
-import { AuthModule } from './auth/auth.module';
-import { PaypalModule } from './paypal/paypal.module';
-import { KrakenModule } from './kraken/kraken.module';
-
-
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { MulterModule } from "@nestjs/platform-express";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ProductModule } from "./products/product.module";
+import { StripeModule } from "./stripe/stripe.module";
+import { NodemailerModule } from "./nodemailer/nodemailer.module";
+import { AuthModule } from "./auth/auth.module";
+import { PaypalModule } from "./paypal/paypal.module";
+import { KrakenModule } from "./kraken/kraken.module";
 
 @Module({
   imports: [
+    MulterModule.register({
+      dest: "./uploads",
+    }),
     ProductModule,
     StripeModule,
     NodemailerModule,
     PaypalModule,
     AuthModule,
     KrakenModule,
-    MongooseModule.forRoot('')],
+    MongooseModule.forRoot(""),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
